1791b62b14081e07f66d0aea9d9439e931f2d92f
sofiane belhia <sofianebelhia150@gmail.com>
2025-09-27 14:08:40 +0100

    fixed map undefined

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { useDebounce } from './custom hooks/useDebounce'
 import './App.css'
 
 function App() {
-  const [value, setValue] = useState('')
+  const [query, setQuery] = useState('')
   const [data, setData] = useState([])
 
   
-  const debouncedValue = useDebounce(value, 1000)
+  const debouncedValue = useDebounce(query, 1000)
 
 
   useEffect(() => {
@@ -24,7 +24,7 @@ function App() {
       }
       const result = await res.json()
       console.log(result.items[0])
-      setData(result)
+      setData(result.items)
     } catch (e) {
       console.error(e)
     }
@@ -35,9 +35,9 @@ function App() {
       <input 
       className='border flex gap-1 items-center rounded-xl px-2' 
       type="text" 
-      onChange={e => setValue(e.target.value)}
+      onChange={e => setQuery(e.target.value)}
       placeholder='Search users' />
-      {debouncedValue && <p className='border rounded-xl w-1/2 p-2'>{data.items.map( item =>{
+      {debouncedValue && <div className='border rounded-xl w-1/2 p-2'>{data.map( item =>{
         return (
           <div className='flex items-center justify-center gap-1 border rounded-xl px-2 mt-1' key={item.id}>
             <img className='absolute ml-5 left-0 w-5 h-5 rounded-full' src={item.avatar_url} alt={item.login} />
@@ -46,7 +46,7 @@ function App() {
             </p>
           </div>
         )
-      })}</p>}
+      })}</div>}
     </>
   )
 }
